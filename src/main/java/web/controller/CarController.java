e4132a4727089ba0107a06f99588cfab935d6d1c
Yuzo1 <97038731+Yuzo1@users.noreply.github.com>
2022-12-09 17:38:04 +0300

    Add files via upload

---
+++ src/main/java/web/controller/CarController.java
@@ -0,0 +1,23 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class CarController {
+    @GetMapping(value ="/cars")
+    public String printCars(@RequestParam(value = "count", defaultValue = "5") int allCars, Model model) {
+        List<Car> cars = Garage.getCars(allCars);
+        model.addAttribute("cars", cars);
+        return "cars";
+    }
+}
