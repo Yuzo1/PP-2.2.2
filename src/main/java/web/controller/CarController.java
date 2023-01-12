bc9b1f8ce708efa902813b17361796a3a9c78a50
Yuzo1 <97038731+Yuzo1@users.noreply.github.com>
2023-01-12 15:28:40 +0300

    Update CarController.java

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,23 +1,25 @@
-package web.controller;
+package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import service.CarService;
-import service.CarServiceImp;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class CarController {
+
+    private final CarService carService;
+
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
     @GetMapping(value ="/cars")
-    public String printCars(@RequestParam(value = "count", defaultValue = "5") int count, ModelMap model) {
-        List<String> messages = new ArrayList<>();
-        CarService carService = new CarServiceImp();
+    public String printCars(@RequestParam(value = "count", required = false) Integer count, ModelMap model) {
         model.addAttribute("cars", carService.severalCars(count));
         return "cars";
     }
 }
-
